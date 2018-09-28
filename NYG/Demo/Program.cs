353d6cc5be6fd974dd2612c9029edbce36b6e89b
BuildTools <unconfigured@null.spigotmc.org>
2018-09-28 21:51:09 +0300

    Gift. ICollection.

--- NYG/Demo/Program.cs
+++ NYG/Demo/Program.cs
@@ -14,16 +14,17 @@ namespace NYG
     {
         static void Main(string[] args)
         {
-            IGift gift = new Gift("Gift 1", new List<ISweetness>());
+          //  IGift gift = new Gift("Gift 1", new List<ISweetness>());
             Candy candy1 = new Candy("Сhupa Chups XXL 4D", 29, 0.65,  370,TypeOfCandy.Bars);
             Candy candy2 = new Candy("Аэрофлотские", 150, 64.3, 483, TypeOfCandy.Chocolate);
             Chocolate choco1 = new Chocolate("Спартак", 75,  48, 540, TypeOfChocolate.Dark);
             Chocolate choco2 = new Chocolate("Любимая Алёнка", 200,  51.5, 550, TypeOfChocolate.Milk);
-            gift.AddSweet(candy1);
-            gift.AddSweet(candy2);
-            gift.AddSweet(choco1);
-            gift.AddSweet(choco2);
-            gift.ShowItems();
+        
+            //gift.AddSweet(candy1);
+            //gift.AddSweet(candy2);
+            //gift.AddSweet(choco1);
+            //gift.AddSweet(choco2);
+            //gift.ShowItems();
         }
     }
 }
--- NYG/Interfaces/IGift.cs
+++ NYG/Interfaces/IGift.cs
@@ -7,15 +7,16 @@ using System.Threading.Tasks;
 
 namespace NYG.Interfaces
 {
-   public interface IGift
+    public interface IGift
     {
+        int CountOfSweet { get; }
+        double GiftWeight { get; }
+        string GiftName { get; }
         IEnumerable<ISweetness> Items { get; }
         IEnumerable<ISweetness> SortSweetnessByWeight();
         IEnumerable<ISweetness> FindSweetnessBySugar(int min, int max);
-        string GiftName { get; }
         void AddSweet(Sweet sweets);
         void RemoveSweet(Sweet sweet);
-        int CountOfSweet { get; }
-        double GiftWeight { get; }
+
     }
 }
